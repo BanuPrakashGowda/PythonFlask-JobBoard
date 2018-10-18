61cbb91f3e5e2376ec74b73abfc88ee43fab75e2
BanuPrakashGowda <38689048+BanuPrakashGowda@users.noreply.github.com>
2018-10-18 13:56:26 +0530

    Update jobs/app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,35 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
+PATH='db/jobs.sqlite'
+
 app=Flask(__name__)
 @app.route("/")
 @app.route("/jobs")
 def jobs():
     return render_template('index.html')
+
+def open_connection():
+    connection=getattr(g,'_connection',None)
+    if connection == None:
+        connection=g._connection=sqlite3.connect(PATH)
+    connection.row_factory=sqlite3.Row
+    return connection
+
+def execute_sql(sql, values=(), comit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql,values)
+    if commit is True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection():
+    connection = getattr(g, '_connection', None)
+    if connection is not None
+        connection.close()
+
+
